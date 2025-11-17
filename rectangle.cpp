ae69aade8786ca1035ad34cbac963aa3ed57afb1
ambatakam <maksymiliancieslak15@wp.pl>
2025-11-17 10:09:24 +0000

    n

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
-
+using namespace std;
 class Rectangle {
-private:
+    private:
     double width;
     double height;
-public:
-    Rectangle(double w, double h):
-    width(w).height(h){};
-    void setWidth(double w) {
+    public:
+    Rectangle(double w, double h)
+    : width(w), height(h){};
+    void setWidth(double w){
         width=w;
     }
-    void setHeight(double h) {
+    void setheight(double h){
         height=h;
     }
-    double getWidth() const{
-        return width
-    }
-    double getHight() const{
-        return height;
-    }
-    double area() const {
-        return width*hight;
-    }
-    double perimeter() const {
-        return 2*(width*hight);
-    }
-    void display() const{
-        cout << "rectangle" << width << " x " << height << endl;
-        cout << "Area" << area() << endl;
-        cout << "Perimeter" << perimeter();
-    }
-
+double getwidth() const{
+    return width;
+}
+double getheight()const{
+    return height;
+}
+double area()const{
+    return width*height;
+}
+double perimeter() const{
+    return 2*(width+height);
+}
+void display(double w, double h) const {
+		cout <<"Rectangle " << width << " x " << height << endl;
+		cout <<"Area " << area() << endl;
+		cout <<"Perimeter "<< perimeter() << endl;
+}
 };
