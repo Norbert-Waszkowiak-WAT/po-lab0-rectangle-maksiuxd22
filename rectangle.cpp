96c9842707c2366a0204aa38a02a1d8d62b6cb2a
ambatakam <maksymiliancieslak15@wp.pl>
2025-11-17 09:57:54 +0000

    zad rectangle

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 
 class Rectangle {
+private:
+    double width;
+    double height;
+public:
+    Rectangle(double w, double h):
+    width(w).height(h){};
+    void setWidth(double w) {
+        width=w;
+    }
+    void setHeight(double h) {
+        height=h;
+    }
+    double getWidth() const{
+        return width
+    }
+    double getHight() const{
+        return height;
+    }
+    double area() const {
+        return width*hight;
+    }
+    double perimeter() const {
+        return 2*(width*hight);
+    }
+    void display() const{
+        cout << "rectangle" << width << " x " << height << endl;
+        cout << "Area" << area() << endl;
+        cout << "Perimeter" << perimeter();
+    }
 
 };
